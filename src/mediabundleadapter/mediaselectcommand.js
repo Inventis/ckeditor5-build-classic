989d51d39433d9f432403436477b078e6e3b773a
Jan Esser <jane@inventis.be>
2019-05-31 14:53:48 +0200

    fix a bug with invalid call to fine optmal image position

--- src/mediabundleadapter/mediaselectcommand.js
+++ src/mediabundleadapter/mediaselectcommand.js
@@ -29,35 +29,3 @@ export default class MediaSelectCommand extends Command {
 		options.element.dispatchEvent(selectMediaEvent);
 	}
 }
-//
-// // Checks if image is allowed by schema in optimal insertion parent.
-// function isImageAllowedInParent( selection, schema ) {
-// 	const parent = getInsertImageParent( selection );
-//
-// 	return schema.checkChild( parent, 'image' );
-// }
-//
-// // Additional check for when the command should be disabled:
-// // - selection is on object
-// // - selection is inside object
-// function checkSelectionWithObject( selection, schema ) {
-// 	const selectedElement = selection.getSelectedElement();
-//
-// 	const isSelectionOnObject = !!selectedElement && schema.isObject( selectedElement );
-// 	const isSelectionInObject = !![ ...selection.focus.getAncestors() ].find( ancestor => schema.isObject( ancestor ) );
-//
-// 	return !isSelectionOnObject && !isSelectionInObject;
-// }
-//
-// // Returns a node that will be used to insert image with `model.insertContent` to check if image can be placed there.
-// function getInsertImageParent( selection ) {
-// 	const insertAt = findOptimalInsertionPosition( selection );
-//
-// 	let parent = insertAt.parent;
-//
-// 	if ( !parent.is( '$root' ) ) {
-// 		parent = parent.parent;
-// 	}
-//
-// 	return parent;
-// }
