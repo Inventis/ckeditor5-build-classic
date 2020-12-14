8d16bc9d0fbe4737bec37c15537fbee4bc99e5b8
Wim Hendrikx <wim@inventis.be>
2020-12-14 16:31:26 +0100

    Add Indent plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -16,6 +16,7 @@ import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
@@ -40,6 +41,7 @@ ClassicEditor.builtinPlugins = [
     ImageCaption,
     ImageStyle,
     ImageToolbar,
+	Indent,
     MediaBundleAdapter,
     Link,
     List,
